3fa6d6eff5b7772893eac80b428eb09617fb66cf
Nico-ehs <nico.ehs05@gmail.com>
2018-09-15 18:44:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
       r[(k+" W/COUPON")][:clearance]=r[k][:clearance]
       r[(k+" W/COUPON")][:price]=el[:cost]
       # puts coupons
-      r[(k+" W/COUPON")][:count]=((cart[k][:count]-r[k][:count])/el[:num])
+      r[(k+" W/COUPON")][:count]=(r[k][:count]))
       # puts r[(k+" W/COUPON")].keys
     end
   end
