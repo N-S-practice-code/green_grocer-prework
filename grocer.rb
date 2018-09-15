c7e5bcd1a23d4f4758c52c9776f222a0601d39a8
Nico-ehs <nico.ehs05@gmail.com>
2018-09-15 17:50:28 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -14,13 +14,11 @@ end
 
 def apply_coupons(cart, coupons)
   r=cart
-  puts r.class
   coupons.each do |el|
-    puts el
     k=el[:item]
     if r.keys.include?(k)
       r[k][:count]+=(0-el[:num])
-      r[(k+" W/COUPON")][]=r[k]
+      r[(k+" W/COUPON")][:clearance]=r[k][:clearance]
       r[(k+" W/COUPON")][:price]=el[:cost]
       r[(k+" W/COUPON")][:count]=1
     end
