66d6904229592302b5ff2a69814fea035d853154
Nico-ehs <nico.ehs05@gmail.com>
2018-09-15 17:39:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,6 @@ def apply_coupons(cart, coupons)
       r[(el1.keys[0]+" W/COUPON")]=r[el1.keys[0]]
       r[(el1.keys[0]+" W/COUPON")][:price]=el1[el1.keys[0][:cost]
       r[(el1.keys[0]+" W/COUPON")][:count]=1
-      
     end
   end
   r
